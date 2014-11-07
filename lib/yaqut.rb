715f0d9253faee88dd467cc1c9ec116b4eef3e83
Collin <collin@remind101.com>
2014-11-07 15:28:54 -0800

    Less downcasing

--- lib/yaqut.rb
+++ lib/yaqut.rb
@@ -8,10 +8,13 @@ module Yaqut
   def self.search(any_field=nil, **search_params)
     raise "Specify at least one search parameter" if any_field.nil? && (search_params.none? || search_params.all? { |param| param.nil? })
     raise "Cannot specify specific fields and a general search term" if any_field && search_params.any?
+    # Case changes can be expensive, only do them once
+    any_field = any_field.downcase if any_field
+    search_params.each{ |key, str| search_params[key] = str.downcase }
     # Not particularly efficient, but there are only ~200
     database.find { |language|
-      (!any_field.nil? && ALL_FIELDS.any? { |field| language.send(field) == any_field.downcase }) ||
-      (search_params.any? && search_params.all? { |field, param| language.send(field) == param.downcase })
+      (!any_field.nil? && ALL_FIELDS.any? { |field| language.send(field) == any_field }) ||
+      (search_params.any? && search_params.all? { |field, param| language.send(field) == param })
     }
   end
 
